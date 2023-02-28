dc688ef31b839989bf915ff05b1ccbe49b2a60c9
KTHYEONG <115347767+KTHYEONG@users.noreply.github.com>
2023-02-28 16:25:42 +0900

    Create 10798.py

---
+++ Python/10798.py
@@ -0,0 +1,12 @@
+## 세로읽기
+import sys
+
+arr = []
+for i in range(5):
+    temp = sys.stdin.readline().strip()
+    arr.append(temp)
+
+for i in range(15):
+    for j in range(5):
+        if i < len(arr[j]):
+            print(arr[j][i], end = '')
