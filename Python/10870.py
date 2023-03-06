3a022336386e67c02911df897719fedef2ece710
KimTaeHyeong <xogud3624@gmail.com>
2023-03-06 21:21:28 +0900

    22.3.6

---
+++ Python/10870.py
@@ -0,0 +1,14 @@
+## 피보나치 수 5
+def fibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1 or n == 2:
+        return 1
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+print(fibonacci(n))
