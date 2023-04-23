b5055a87eadf15a2bf3631cfa875b849347574c5
KimTaeHyeong <xogud3624@gmail.com>
2023-04-23 14:03:31 +0900

    23.4.23

--- JAVA/src/_2562.java
+++ JAVA/src/_2562.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class $2562 {
+public class _2562 {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
 
