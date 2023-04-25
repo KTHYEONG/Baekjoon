6f868425a37b1d8834f1cd70b59fe6ea79f76580
KimTaeHyeong <xogud3624@gmail.com>
2023-04-25 22:38:05 +0900

    22.4.25

---
+++ JAVA/src/_1152.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class _1152 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = new String();
+        
+        // 문자열 입력 받고 띄어쓰기를 기준으로 토큰으로 분리
+        str = sc.nextLine();
+        StringTokenizer ST = new StringTokenizer(str);
+        System.out.println(ST.countTokens());
+    }
+}
