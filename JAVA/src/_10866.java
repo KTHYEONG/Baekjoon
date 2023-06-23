eb4242f35e303ce2c2ce57219b6978793a5e969f
KimTaeHyeong <xogud3624@gmail.com>
2023-06-23 21:35:59 +0900

    23.6.23

---
+++ JAVA/src/_10866.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class _10866 {
+    public static void main(String[] args) throws IOException {
+        Deque<Integer> deque = new LinkedList<>();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            String[] command = br.readLine().split(" ");
+            switch (command[0]) {
+                case "push_front":
+                    deque.addFirst(Integer.parseInt(command[1]));
+                    break;
+                case "push_back":
+                    deque.addLast(Integer.parseInt(command[1]));
+                    break;
+                case "pop_front":
+                    System.out.println(deque.isEmpty() ? -1 : deque.pollFirst());
+                    break;
+                case "pop_back":
+                    System.out.println(deque.isEmpty() ? -1 : deque.pollLast());
+                    break;
+                case "size":
+                    System.out.println(deque.size());
+                    break;
+                case "empty":
+                    System.out.println(deque.isEmpty() ? 1 : 0);
+                    break;
+                case "front":
+                    System.out.println(deque.isEmpty() ? -1 : deque.peekFirst());
+                    break;
+                case "back":
+                    System.out.println(deque.isEmpty() ? -1 : deque.peekLast());
+            }
+        }
+    }
+}
