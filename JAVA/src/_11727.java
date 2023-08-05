1e266100e0d24169abb3ef9cafa8cf9970010baa
KimTaeHyeong <xogud3624@gmail.com>
2023-08-05 13:28:17 +0900

    23.8.5

---
+++ JAVA/src/_11727.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+// 2xn 타일링 2
+public class _11727 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n + 1];
+        arr[0] = arr[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            arr[i] = (arr[i - 1] + 2 * arr[i - 2]) % 10007;
+        }
+        bw.write(arr[n] + "");
+
+        bw.flush();
+        bw.close();
+    }
+}
