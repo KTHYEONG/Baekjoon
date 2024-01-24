167301bd1cd5fbe533e2b025e5e5217c67a20b28
KimTaeHyeong <xogud3624@gmail.com>
2024-01-24 19:50:58 +0900

    23.01.24 - 부분합 - dp - StringBuilder & BufferedReader 이용하기

---
+++ JAVA/src/_11660.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 구간 합 구하기 5
+public class _11660 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[][] arr = new int[N + 1][N + 1];
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= N; j++) {
+                arr[i][j] = arr[i][j - 1] + Integer.parseInt(st.nextToken());
+            }
+        }
+
+        StringBuilder ans = new StringBuilder();
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+
+            int sum = 0;
+            for (int j = x1; j <= x2; j++) {
+                sum = sum + (arr[j][y2] - arr[j][y1 - 1]);
+            }
+            ans.append(sum + "\n");
+        }
+        System.out.println(ans);
+    }
+}
